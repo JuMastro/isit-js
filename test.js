6df9e67a1b175676598e1c48a561c4c3eb780f4c
JuMastro <mastromonaco.julien@gmail.com>
2018-02-13 20:05:33 +0100

    Removed usless files

--- test.js
+++
@@ -1,8 +0,0 @@
-const izit = require('./lib/index.js')
-
-const test = ['test', 'test2']
-const check = izit(test).array().min(100).max(1).length(100)
-
-if (check.hasErrors()) {
-  console.log(check.getErrors())
-}
