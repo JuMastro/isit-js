fd498c10c5503e7f5e307613d4a905c9c22663b1
JuMastro <mastromonaco.julien@gmail.com>
2018-02-13 14:14:18 +0100

    Add array checks (min, max & length)

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const izit = require('./lib/index.js')
 
-const check = izit('testt').array()
+const check = izit(['test', 'test2']).array().min(100).max(1).length(100)
 
 if (check.hasErrors()) {
   console.log(check.getErrors())
